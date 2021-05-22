809f76de0e71e7931e5e5e128b5867aee1a23deb
Hidnish <hideonishimura@live.it>
2021-05-22 14:18:44 +0000

    Set up email API

---
+++ assets/js/email.js
@@ -0,0 +1,22 @@
+function sendMail(contactForm) {
+    emailjs.send("service_lso3e7r", "kyoto_daynight", {
+            "from_firstname": contactForm.firstname.value,
+            "from_lastname": contactForm.lastname.value,
+            "from_email": contactForm.email.value,
+            "request": contactForm.request.value
+        })
+        .then(
+            function (response) {
+                let submitButton = document.getElementById('btnSubmit');
+                submitButton.innerHTML = "Thank you!";
+                submitButton.style.backgroundColor = "rgba(42,94,40,0.9)"
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                alert("Sorry, something went wrong. Please fill out the form and click submit again", error);
+                console.log("FAILED", error);
+            })
+
+    document.getElementById('fillForm').reset();
+    return false; //This is to prevent the page from refreshing when you submit your form    
+}
