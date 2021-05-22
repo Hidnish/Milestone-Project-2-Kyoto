959128086b2e7b0aac15d7c1d516591327d94742
Hidnish <hideonishimura@live.it>
2021-05-22 11:24:23 +0000

    Add contact form

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,6 +4,7 @@ let layerNight = new L.LayerGroup();
 let map = L.map('map', {
     center: [35.0116, 135.7681],
     zoom: 11,
+    minZoom: 5,
     layers: [layerDay, layerNight]
 });
 
@@ -18,44 +19,45 @@ let mapDark = L.tileLayer(`https://api.maptiler.com/maps/jp-mierune-dark/{z}/{x}
 });
 
 let locationBox = document.getElementById('switch');
+let contactBox = document.getElementById('switch2');
 
 // --------------- Locations grouped into objects -----------------
 
 const LocationsVar = [
 
-        // Historical sites
+    // Historical sites
     {
         name: "Nijo Castle",
         center: [35.014168, 135.7474],
-        zoom: 8,
+        zoom: 14,
         group: 'historical',
         id: 'site1',
     },
     {
         name: "Fushimi Inari Taisha",
         center: [34.9671, 135.7727],
-        zoom: 8,
+        zoom: 14,
         group: 'historical',
         id: 'site2',
     },
     {
         name: "Kyoto Imperial Palace",
         center: [35.0254, 135.7621],
-        zoom: 8,
+        zoom: 14,
         group: 'historical',
         id: 'site3',
     },
     {
         name: "Kinkaku-ji Temple",
         center: [35.0394, 135.7292],
-        zoom: 8,
+        zoom: 14,
         group: 'historical',
         id: 'site4',
     },
     {
         name: "To-ji Temple",
         center: [34.9806, 135.7478],
-        zoom: 8,
+        zoom: 14,
         group: 'historical',
         id: 'site5',
     },
@@ -65,28 +67,28 @@ const LocationsVar = [
     {
         name: "Sogenchi Pond Garden",
         center: [35.0156, 135.6734],
-        zoom: 8,
+        zoom: 14,
         group: 'garden',
         id: 'park1',
     },
     {
         name: "Haradani Garden",
         center: [35.0441, 135.7141],
-        zoom: 8,
+        zoom: 14,
         group: 'garden',
         id: 'park2',
     },
     {
         name: "Ryōan-ji",
         center: [35.0345, 135.7183],
-        zoom: 8,
+        zoom: 14,
         group: 'garden',
         id: 'park3',
     },
     {
         name: "Maruyama Park",
         center: [35.0036, 135.7805],
-        zoom: 8,
+        zoom: 14,
         group: 'garden',
         id: 'park4',
     },
@@ -96,43 +98,91 @@ const LocationsVar = [
     {
         name: "Saishuan Shiraki",
         center: [35.0165, 135.7661],
-        zoom: 8,
+        zoom: 14,
         group: 'restaurants',
         id: 'rest1',
     },
     {
         name: "Okonomiyaki Katsu",
         center: [35.0303, 135.7206],
-        zoom: 8,
+        zoom: 14,
         group: 'restaurants',
         id: 'rest2',
     },
     {
         name: "Sugarhill Kyoto",
         center: [34.9982, 135.7671],
-        zoom: 8,
+        zoom: 14,
         group: 'restaurants',
         id: 'rest3',
     },
     {
         name: "Kyoto Gion Mikaku",
         center: [35.0051, 135.7726],
-        zoom: 8,
+        zoom: 14,
         group: 'restaurants',
         id: 'rest4',
     },
+
+    // Bars 
+
+    {
+        name: "L'Escamoteur",
+        center: [35.0018, 135.7699],
+        zoom: 14,
+        group: 'bars',
+        id: 'bar1',
+    },
+    {
+        name: "Ki Bar",
+        center: [35.0082, 135.7710],
+        zoom: 14,
+        group: 'bars',
+        id: 'bar2',
+    },
+    {
+        name: "Kyoto Star Bar",
+        center: [35.0073, 135.7709],
+        zoom: 14,
+        group: 'bars',
+        id: 'bar3',
+    },
+    {
+        name: "Butterfly",
+        center: [35.0079, 135.7697],
+        zoom: 14,
+        group: 'bars',
+        id: 'bar4',
+    },
 ];
 
-
-
-// Import Customised markers 
+// Import Custom markers 
 const greenIcon = L.icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+});
+
+const goldIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});
+
+const redIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});
+
+const greyIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
 });
 
 //--------------------------------------------------------------------//
@@ -202,12 +252,11 @@ for (let button of buttonDayNite) {
             if (button.id === 'button-day') {
                 if (LocationsVar[i].group === 'historical') {
                     marker = new L.marker([LocationsVar[i].center[0], LocationsVar[i].center[1]], {
-                        id: LocationsVar[i].id,
+                        icon: greyIcon,
                     });
                 } else if (LocationsVar[i].group === 'garden') {
                     marker = new L.marker([LocationsVar[i].center[0], LocationsVar[i].center[1]], {
                         icon: greenIcon,
-                        id: LocationsVar[i].id,
                     });
                 }
                 marker.bindPopup(LocationsVar[i].name);
@@ -217,12 +266,11 @@ for (let button of buttonDayNite) {
             } else if (button.id === 'button-night') {
                 if (LocationsVar[i].group === 'restaurants') {
                     marker = new L.marker([LocationsVar[i].center[0], LocationsVar[i].center[1]], {
-                        id: LocationsVar[i].id,
+                        icon: redIcon,
                     });
                 } else if (LocationsVar[i].group === 'bars') {
                     marker = new L.marker([LocationsVar[i].center[0], LocationsVar[i].center[1]], {
-                        icon: greenIcon,
-                        id: LocationsVar[i].id,
+                        icon: goldIcon,
                     });
                 }
                 marker.bindPopup(LocationsVar[i].name);
@@ -235,9 +283,8 @@ for (let button of buttonDayNite) {
 
 // Switch map to day/night mode and change the markers(locations) accordingly 
 function switcher(button) {
-
-    map.removeLayer(singleMarker);
-
+    map.removeLayer(singleMarker).setView([35.0116, 135.7381], 12);;
+    
     if (button.id === "button-day") {
         mapDark.remove();
         mapLight.addTo(map);
@@ -245,6 +292,7 @@ function switcher(button) {
         layerDay.addLayer(marker);
         layerDay.addTo(map);
         locationBox.classList.remove('night-mode');
+        contactBox.classList.remove('backg-contact-night');
     } else {
         mapLight.remove();
         mapDark.addTo(map);
@@ -252,6 +300,7 @@ function switcher(button) {
         layerNight.addLayer(marker);
         layerNight.addTo(map);
         locationBox.classList.add('night-mode');
+        contactBox.classList.add('backg-contact-night');
     };
 };
 
@@ -259,17 +308,18 @@ function switcher(button) {
 
 let locationButtons = $('.locations-list li').children();
 let singleMarker = {};
-// Inspired by ....
+
 for (let i = 0; i < locationButtons.length; i++) {
     locationButtons[i].addEventListener('click', function () {
         map.removeLayer(layerDay);
         map.removeLayer(layerNight);
         let buttonData = locationButtons[i].dataset.location;
 
+        // Inspired by ....
         const findMapLocation = LocationsVar.find(
             (name) => name.id === buttonData);
 
-        map.flyTo(findMapLocation.center);
+        map.flyTo(findMapLocation.center, findMapLocation.zoom);
         if (findMapLocation.center) {
             map.removeLayer(singleMarker);
             singleMarker = L.marker(findMapLocation.center).bindPopup(LocationsVar[i].name).addTo(map).openPopup();
