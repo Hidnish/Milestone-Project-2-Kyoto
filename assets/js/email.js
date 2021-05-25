878d2c609927be1de1d7d6466143d423cd223d5c
Hidnish <hideonishimura@live.it>
2021-05-25 14:13:36 +0000

    Transfer emailsjs.init function from index.html

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,3 +1,7 @@
+(function () {
+    emailjs.init("user_HFAbZtQk3xr0kgOSlVrJi");
+})();
+
 function sendMail(contactForm) {
     emailjs.send("service_lso3e7r", "kyoto_daynight", {
             "from_firstname": contactForm.firstname.value,
@@ -9,7 +13,8 @@ function sendMail(contactForm) {
             function (response) {
                 let submitButton = document.getElementById('btnSubmit');
                 submitButton.innerHTML = "Thank you!";
-                submitButton.style.backgroundColor = "rgba(42,94,40,0.9)"
+                submitButton.style.backgroundColor = "rgba(42,94,40,0.9)";
+                alert("Your message has been successfully sent! We will get in touch as soon as possible.");
                 console.log("SUCCESS", response);
             },
             function (error) {
