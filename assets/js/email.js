9804db6c2b126a728cf2b6fb402cdd31a1c8e803
Hidnish <hideonishimura@live.it>
2021-05-29 09:26:44 +0000

    Add descriptions to relevant sections

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,6 +2,7 @@
     emailjs.init("user_HFAbZtQk3xr0kgOSlVrJi");
 })();
 
+// Functions based on the Code Institute tutorial: Rosie - Resume Project
 function sendMail(contactForm) {
     emailjs.send("service_lso3e7r", "kyoto_daynight", {
             "from_firstname": contactForm.firstname.value,
@@ -10,6 +11,7 @@ function sendMail(contactForm) {
             "request": contactForm.request.value
         })
         .then(
+            // If the message is successfully sent 
             function (response) {
                 let submitButton = document.getElementById('btnSubmit');
                 submitButton.innerHTML = "Thank you!";
@@ -17,11 +19,12 @@ function sendMail(contactForm) {
                 alert("Your message has been successfully sent! We will get in touch as soon as possible.");
                 console.log("SUCCESS", response);
             },
+            // If the message does not send
             function (error) {
                 alert("Sorry, something went wrong. Please fill out the form and click submit again", error);
                 console.log("FAILED", error);
             });
 
-    document.getElementById('fillForm').reset();
+    document.getElementById('fillForm').reset(); //Reset form once the message has been sent
     return false; //This is to prevent the page from refreshing when you submit your form    
 }
